ba9d4f2ed153a29b4f2885e1464f12983cf602e8
Florian-Bats <81744660+Florian-Bats@users.noreply.github.com>
2023-12-13 14:59:36 +0100

    adding content

--- MAD/App.js
+++ MAD/App.js
@@ -1,20 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { React, StyleSheet, Text, View, Pressable } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.mainscreen}>
+      <View style={styles.header}>
+      <StatusBar style={styles.header}/>
+      </View>
+      <Pressable style={styles.btnmelding}>
+        <Text style={styles.btnmeldingtxt}>+</Text>
+      </Pressable>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
+  header: {
+    alignContent: 'center',
+  },
+  mainscreen: {
+    display: 'flex',
+    flexDirection: 'collumn',
+    alignContent: 'center',
+  },
+  btnmelding: {
+    maxWidth: '50%',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#ff0000',
+    borderRadius: 4.0,
+  },
+  bntmeldingtxt: {
+    alignContent: 'center',
+    fontSize: '20',
+    lineHeight: '21',
+    fontWeight: 'bold',
+    letterSpacing: '0.25',
+    color: '#fff',
   },
 });
